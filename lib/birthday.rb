30f995e32b30028800ff145dbe0ad9e546f897f6
Oscar Shoudt <shoudto@gmail.com>
2019-08-01 16:11:40 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -17,5 +17,6 @@ def age_appropriate_birthday(birthday_kids)
   birthday_kids.each do |kid_name, age|
     if age < 12
       puts "Happy Birthday #{kid_name}! You are now #{age} years old!"
+    end 
   end
 end
