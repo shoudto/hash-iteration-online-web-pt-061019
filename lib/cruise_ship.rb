b26e5a1bfd28437891cb9b31d7544ca07da449c1
Oscar Shoudt <shoudto@gmail.com>
2019-08-01 15:59:58 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -12,7 +12,7 @@ def select_winner(passengers)
   winner = ""
   passengers.each do |suite, name|
     if suite == :suite_a && name.start_with?("A")
-      name
+      winner = name
     end
   end
 end
