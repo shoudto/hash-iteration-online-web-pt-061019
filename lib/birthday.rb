59833986d969d0b29054cd0437d13e1e2d84f79b
Oscar Shoudt <shoudto@gmail.com>
2019-08-01 15:50:22 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -1,11 +1,12 @@
+require 'pry'
+
 # birthday_kids = {
-#   "Timmy" => 9, 
-#   "Sarah" => 6, 
+#   "Timmy" => 9,
+#   "Sarah" => 6,
 #   "Amanda" => 27
 # }
 
 def happy_birthday(birthday_kids)
   # add your code snippet here!
+  binding.pry 
 end
-
-
